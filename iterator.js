88ed9b62a04cc77954780ef90ab22ea884a7c5d2
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-11 18:34:02 +0200

    feat:add example

--- iterator.js
+++ iterator.js
@@ -1,4 +1,4 @@
-class ArrayIterator {
+class Iterator {
 	constructor(el) {
 		this.index = 0;
 		this.elements = el;
@@ -28,3 +28,17 @@ class ObjectIterator {
 		return this.index < this.keys.length;
 	}
 };
+
+const collection = new Iterator(["Audi","BMW","Tesla","Mersedes"])
+while (collection.hasNext()){
+	console.log(collection.next());
+}
+
+const autos ={
+	audi:{model:"Audi",color:"black",price:'20000'},
+	bmw:{model:"BMW",color:"white",price:'15000'},
+	tesla:{model:"Tesla",color:"blue",price:'10000'}
+}
+const collectionObj = new ObjectIterator(autos)
+while (collectionObj.hasNext()){
+	console.log(collectionObj.next())}
